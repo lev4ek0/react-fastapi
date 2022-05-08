75a2f2e19933e355c47aaac24363dc0e1985ce6d
Lev Belous <leva22.08.01@inbox.ru>
2022-05-08 23:44:58 +0300

    test fastapi + aiogram heroku

--- app.py
+++ app.py
@@ -27,31 +27,6 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    TOKEN = os.getenv('BOT_TOKEN')
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher(bot)
-
-    HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-    # webhook settings
-    WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-    WEBHOOK_PATH = f'/webhook/{TOKEN}'
-    WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-    # webserver settings
-    WEBAPP_HOST = '0.0.0.0'
-    WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
     return "ok"
 
 
@@ -67,6 +42,13 @@ async def on_shutdown(dispatcher):
 async def echo(message: types.Message):
     await message.answer(message.text)
 
-
-
-
+logging.basicConfig(level=logging.INFO)
+start_webhook(
+    dispatcher=dp,
+    webhook_path=WEBHOOK_PATH,
+    skip_updates=True,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    host=WEBAPP_HOST,
+    port=WEBAPP_PORT,
+)
