46bc1edd5258d36c816bc21f8a2fbdb5474c7f9c
Lev Belous <leva22.08.01@inbox.ru>
2022-05-08 23:47:40 +0300

    test fastapi + aiogram heroku

--- app.py
+++ app.py
@@ -42,13 +42,15 @@ async def on_shutdown(dispatcher):
 async def echo(message: types.Message):
     await message.answer(message.text)
 
-logging.basicConfig(level=logging.INFO)
-start_webhook(
-    dispatcher=dp,
-    webhook_path=WEBHOOK_PATH,
-    skip_updates=True,
-    on_startup=on_startup,
-    on_shutdown=on_shutdown,
-    host=WEBAPP_HOST,
-    port=WEBAPP_PORT,
-)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
