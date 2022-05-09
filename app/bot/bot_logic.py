58e8894d57e17045e9ef7a8e89a74abed154f26b
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 20:14:26 +0300

    last version

--- app/bot/bot_logic.py
+++ app/bot/bot_logic.py
@@ -17,65 +17,32 @@ async def send_welcome(message: types.Message):
                         "Just send me it or send me link.")
 
 
-@dp.message_handler(content_types=['photo', 'document', 'text'])
-async def handle_photos(message: types.Message):
-    print(message.text)
+async def get_url(message: types.Message) -> str:
+    url = message.text
     if len(message.photo) != 0:
-        url = message.photo[-1].get_url()
-        print(url)
+        url = await message.photo[-1].get_url()
     if message.document is not None:
-        url = message.document.get_url()
-        print(url)
-    await message.reply(
-        f'Faces found: {1}'
-    )
+        url = await message.document.get_url()
+    return url
 
 
-# def find_faces(url: str) -> int:
-#     f = requests.get(url)
-#     if f.status_code != 200:
-#         raise Exception
-#     image = face_recognition.load_image_file(BytesIO(f.content))
-#     face_locations = face_recognition.face_locations(image)
-#     return len(face_locations)
-#
-#
-# @dp.message_handler()
-# async def handle_urls(message: types.Message):
-#     url = message.text
-#     try:
-#         amount_of_faces = find_faces(url)
-#         await message.reply(
-#             f'Faces found: {amount_of_faces}'
-#         )
-#     except requests.exceptions.MissingSchema:
-#         await message.reply(
-#             f'Url is wrong'
-#         )
-#     except UnidentifiedImageError:
-#         await message.reply(
-#             f'File is not image'
-#         )
-#
-#
-# @dp.message_handler(content_types=['photo'])
-# async def handle_photos(message: types.Message):
-#     url = await message.photo[-1].get_url()
-#     amount_of_faces = find_faces(url)
-#     await message.reply(
-#         f'Faces found: {amount_of_faces}'
-#     )
-#
-#
-# @dp.message_handler(content_types=['document'])
-# async def handle_documents(message: types.Message):
-#     try:
-#         url = await message.document.get_url()
-#         amount_of_faces = find_faces(url)
-#         await message.reply(
-#             f'Faces found: {amount_of_faces}'
-#         )
-#     except UnidentifiedImageError:
-#         await message.reply(
-#             f'File is not image'
-#         )
+async def find_faces(url: str) -> int:
+    f = requests.get(url)
+    image = face_recognition.load_image_file(BytesIO(f.content))
+    face_locations = face_recognition.face_locations(image)
+    return len(face_locations)
+
+
+@dp.message_handler(content_types=['text', 'photo', 'document'])
+async def handle_photos(message: types.Message):
+    url = await get_url(message)
+    try:
+        amount_of_faces = await find_faces(url)
+        response = f'Faces found: {amount_of_faces}'
+    except requests.exceptions.MissingSchema:
+        response = f'Url is wrong'
+    except UnidentifiedImageError:
+        response = f'File is not image'
+    except Exception:
+        response = 'Smth went wrong'
+    await message.reply(response)
