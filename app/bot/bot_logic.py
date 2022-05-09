d38dbd1eb6e7eec49f771042cecbfd69cdbd3915
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 18:13:34 +0300

    .

--- app/bot/bot_logic.py
+++ app/bot/bot_logic.py
@@ -14,11 +14,10 @@ async def echo(message: types.Message):
 
 @dp.message_handler(content_types=['photo'])
 async def handle_photos(message: types.Message):
-    url = await message.photo[-1].get_url()
-    image = face_recognition.load_image_file(url)
+    file = await message.photo[-1].get_file()
+    image = face_recognition.load_image_file(file)
     face_locations = face_recognition.face_locations(image)
     await message.answer(
         f'Size: {message.photo[-1].file_size}\n'
-        f'Url: {url}\n'
         f'\"Лиц найдено:\", {len(face_locations)}'
     )
