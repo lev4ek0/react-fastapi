77cff1b60c63084e9dffa440fe268aba05e1526a
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 16:34:06 +0300

    .

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
     TOKEN: str = os.getenv('BOT_TOKEN')
     WEBHOOK_HOST: str = os.getenv('WEBHOOK_HOST')
-    SECRET: str = TOKEN.replace(':', '')
+    SECRET: str = TOKEN
     WEBHOOK_PATH: str = f'webhook/{SECRET}'
     WEBHOOK_URL: str = f'{WEBHOOK_HOST}{API_V1_STR}/bot/{WEBHOOK_PATH}'
 
