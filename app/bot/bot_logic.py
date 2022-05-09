6762da80ee9a6a73378a6efa2d8f16e4e55d6de6
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 19:55:05 +0300

    .

--- app/bot/bot_logic.py
+++ app/bot/bot_logic.py
@@ -17,15 +17,29 @@ async def send_welcome(message: types.Message):
                         "Just send me it or send me link.")
 
 
-def find_faces(url: str) -> int:
-    f = requests.get(url)
-    if f.status_code != 200:
-        raise Exception
-    image = face_recognition.load_image_file(BytesIO(f.content))
-    face_locations = face_recognition.face_locations(image)
-    return len(face_locations)
+@dp.message_handler(content_types=['photo', 'document', 'text'])
+async def handle_photos(message: types.Message):
+    print(message.text)
+    if len(message.photo) != 0:
+        url = message.photo[-1].get_url()
+        print(url)
+    print(message.document)
+    url = message.document.get_url()
+    print(url)
+    await message.reply(
+        f'Faces found: {1}'
+    )
 
 
+# def find_faces(url: str) -> int:
+#     f = requests.get(url)
+#     if f.status_code != 200:
+#         raise Exception
+#     image = face_recognition.load_image_file(BytesIO(f.content))
+#     face_locations = face_recognition.face_locations(image)
+#     return len(face_locations)
+#
+#
 # @dp.message_handler()
 # async def handle_urls(message: types.Message):
 #     url = message.text
@@ -42,20 +56,17 @@ def find_faces(url: str) -> int:
 #         await message.reply(
 #             f'File is not image'
 #         )
-
-
-@dp.message_handler(content_types=['photo', 'document', 'text'])
-async def handle_photos(message: types.Message):
-    print(message.text)
-    url = message.photo[-1].get_url()
-    print(url)
-    url = message.document.get_url()
-    print(url)
-    await message.reply(
-        f'Faces found: {1}'
-    )
-
-
+#
+#
+# @dp.message_handler(content_types=['photo'])
+# async def handle_photos(message: types.Message):
+#     url = await message.photo[-1].get_url()
+#     amount_of_faces = find_faces(url)
+#     await message.reply(
+#         f'Faces found: {amount_of_faces}'
+#     )
+#
+#
 # @dp.message_handler(content_types=['document'])
 # async def handle_documents(message: types.Message):
 #     try:
