a922da65f151864f7935f6f1741535516189c913
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 17:51:35 +0300

    .

--- app/bot/bot_logic.py
+++ app/bot/bot_logic.py
@@ -1,4 +1,5 @@
 from aiogram import Dispatcher, Bot, types
+import face_recognition
 
 from . import settings
 
@@ -14,7 +15,10 @@ async def echo(message: types.Message):
 @dp.message_handler(content_types=['photo'])
 async def handle_photos(message: types.Message):
     url = await message.photo[-1].get_url()
+    image = face_recognition.load_image_file(url)
+    face_locations = face_recognition.face_locations(image)
     await message.answer(
         f'Size: {message.photo[-1].file_size}\n'
         f'Url: {url}\n'
+        f'\"Лиц найдено:\", {len(face_locations)}'
     )
