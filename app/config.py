e71170c533b186f3f10cdb23db89704f4556938c
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 16:43:17 +0300

    .

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from functools import lru_cache
+from urllib.parse import quote
 
 from pydantic import BaseSettings
 
@@ -8,7 +9,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
     TOKEN: str = os.getenv('BOT_TOKEN')
     WEBHOOK_HOST: str = os.getenv('WEBHOOK_HOST')
-    SECRET: str = TOKEN
+    SECRET: str = quote(TOKEN)
     WEBHOOK_PATH: str = f'/webhook/{SECRET}'
     WEBHOOK_URL: str = f'{WEBHOOK_HOST}{API_V1_STR}/bot{WEBHOOK_PATH}'
 
