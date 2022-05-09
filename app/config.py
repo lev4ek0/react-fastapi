a887f88239b398d51277dfcbc0c7f0915306ef09
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 14:14:00 +0300

    restructure

--- app/config.py
+++ app/config.py
@@ -11,8 +11,8 @@ class Settings(BaseSettings):
     TOKEN: str = os.getenv('BOT_TOKEN')
     WEBHOOK_HOST: str = os.getenv('WEBHOOK_HOST')
     SECRET: str = os.getenv('SECRET')
-    WEBHOOK_PATH: str = f'/api/bot/webhook/{SECRET}'
-    WEBHOOK_URL: str = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+    WEBHOOK_PATH: str = f'/webhook/{SECRET}'
+    WEBHOOK_URL: str = f'{WEBHOOK_HOST}/api/bot{WEBHOOK_PATH}'
 
 
 class DevelopmentConfig(Settings):
