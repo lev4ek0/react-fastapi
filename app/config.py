64d831798ce77523c7319115c97ead4c53c4fb8b
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 16:12:19 +0300

    .

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     TOKEN: str = os.getenv('BOT_TOKEN')
     WEBHOOK_HOST: str = os.getenv('WEBHOOK_HOST')
-    SECRET: str = TOKEN
+    SECRET: str = TOKEN.replace(':', '')
     WEBHOOK_PATH: str = f'/webhook/{SECRET}'
     WEBHOOK_URL: str = f'{WEBHOOK_HOST}/api/bot{WEBHOOK_PATH}'
 
