265d42a26d41a263bd781d5e2a9dfd8a1a86cba3
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 15:54:41 +0300

    .

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 import os
+from functools import lru_cache
+
 from dotenv import load_dotenv
 from pydantic import BaseSettings
 
@@ -8,10 +10,11 @@ load_dotenv()
 class Settings(BaseSettings):
     TOKEN: str = os.getenv('BOT_TOKEN')
     WEBHOOK_HOST: str = os.getenv('WEBHOOK_HOST')
-    SECRET: str = os.getenv('SECRET')
+    SECRET: str = TOKEN.replace(':', '')
     WEBHOOK_PATH: str = f'/webhook/{SECRET}'
     WEBHOOK_URL: str = f'{WEBHOOK_HOST}/api/bot{WEBHOOK_PATH}'
 
 
+@lru_cache
 def get_config():
     return Settings()
