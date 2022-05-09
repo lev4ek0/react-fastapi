3e5296da73033a29a7bc197bb26caa736da3ac9b
Lev Belous <leva22.08.01@inbox.ru>
2022-05-09 14:21:12 +0300

    restructure

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,6 @@ def create_app(config="dev"):
 
     @app.on_event('shutdown')
     async def on_shutdown():
-        await bot.get_session().close()
+        await bot.session.close()
 
     return app
